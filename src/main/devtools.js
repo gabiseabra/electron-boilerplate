fcb4bddd45b7e3ce7d61a129b450aac733fa1ce1
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-07-10 19:31:42 -0300

    Fixed dev imports in production build

--- src/main/devtools.js
+++ src/main/devtools.js
@@ -1,13 +1,21 @@
+import { app } from "electron"
+import debug from "electron-debug"
 import installExtension, {
 	REACT_DEVELOPER_TOOLS,
 	REDUX_DEVTOOLS,
 	REACT_PERF
 } from "electron-devtools-installer"
 
-export default async function install(forceDownload) {
-	await Promise.all([
-		installExtension(REACT_DEVELOPER_TOOLS, forceDownload),
-		installExtension(REDUX_DEVTOOLS, forceDownload),
-		installExtension(REACT_PERF, forceDownload)
-	])
+const installDevtools = (forceDownload) => 	Promise.all([
+	installExtension(REACT_DEVELOPER_TOOLS, forceDownload),
+	installExtension(REDUX_DEVTOOLS, forceDownload),
+	installExtension(REACT_PERF, forceDownload)
+])
+
+export default function init(forceDownload) {
+	debug()
+	app.once("ready", () => {
+		console.log("==> App is running in development mode")
+		installDevtools(forceDownload)
+	})
 }
--- src/main/index.js
+++ src/main/index.js
@@ -1,25 +1,15 @@
 import { app, Menu } from "electron"
-import debug from "electron-debug"
 import { setGlobals } from "./globals"
 import template from "./menu"
-import devtools from "./devtools"
 import windows from "../windows"
 
 if(process.env.NODE_ENV === "development") {
-	debug()
+	require("./devtools").default()
 }
 
-app.once("ready", async () => {
-	// Set global vars
+app.once("ready", () => {
 	setGlobals()
-	// Install dev tools
-	if(process.env.NODE_ENV === "development") {
-		console.info("==> App running in development mode")
-		await devtools(process.env.DL_EXTENSIONS === "true")
-	}
-	// Open main window
 	windows.main()
-	// Set application menu
 	Menu.setApplicationMenu(
 		Menu.buildFromTemplate(template)
 	)

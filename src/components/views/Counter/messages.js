0f8308c94b9ffd050b48d76ba65e258a7f34d582
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-07-11 14:11:49 -0300

    Added counter messages

---
+++ src/components/views/Counter/messages.js
@@ -0,0 +1,24 @@
+import { defineMessages } from "react-intl"
+
+export default defineMessages({
+	increment: {
+		id: "views.Counter.increment",
+		description: "Increment button tooltip",
+		defaultMessage: "Increment"
+	},
+	decrement: {
+		id: "views.Counter.decrement",
+		description: "Decrement button tooltip",
+		defaultMessage: "Decrement"
+	},
+	incrementAsync: {
+		id: "views.Counter.incrementAsync",
+		description: "Increment async button tooltip",
+		defaultMessage: "Increment Async"
+	},
+	decrementAsync: {
+		id: "views.Counter.decrementAsync",
+		description: "Decrement async button tooltip",
+		defaultMessage: "Decrement Async"
+	}
+})

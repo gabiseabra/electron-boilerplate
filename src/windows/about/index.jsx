3265b2f37b00595ad1b922441eb56bebb1b4a749
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-08-30 17:11:40 -0300

    Fixed about entry script

--- src/windows/about/index.jsx
+++ src/windows/about/index.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { HashRouter as Router } from "react-router-dom"
-import { About, Provider } from "../../containers/app"
+import { Provider } from "../../containers/app"
+import { About } from "../../containers/views"
+
 
 ReactDOM.render(
 	<Provider>

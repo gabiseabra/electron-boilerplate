301b1f8a96c1adff53899e2b6fa03a6e38d57e12
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-08-31 19:14:04 -0300

    Fixed hot module replacement

--- src/windows/main/index.jsx
+++ src/windows/main/index.jsx
@@ -2,22 +2,36 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { HashRouter as Router } from "react-router-dom"
 import { Provider } from "../../containers/app"
-import { Shell } from "../../containers/views"
 import createStore from "../../redux/store"
-import createSaga from "../../redux/saga"
-import routes from "./routes"
+
+let task
 
 const store = createStore()
 
-store.runSaga(createSaga())
+const runSaga = () => {
+	const createSaga = require("../../redux/saga").default
+	if(task) task.cancel()
+	task = store.runSaga(createSaga())
+}
 
-ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<Shell>{routes}</Shell>
-		</Router>
-	</Provider>,
-	document.getElementById("app")
-)
+const render = () => {
+	const { Shell } = require("../../containers/views")
+	const routes = require("./routes").default
+	ReactDOM.render(
+		<Provider store={store}>
+			<Router>
+				<Shell>{routes}</Shell>
+			</Router>
+		</Provider>,
+		document.getElementById("app")
+	)
+}
 
-if(module.hot) module.hot.accept()
+render()
+
+runSaga()
+
+if(module.hot) {
+	module.hot.accept("../../redux/saga", runSaga)
+	module.hot.accept([ "../../containers/views", "./routes" ], render)
+}

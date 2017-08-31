f67d95687b8fc971041bbc19c3a089791c76cde4
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-08-30 21:47:42 -0300

    Added postcss resolver

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,29 @@
 const path = require("path")
+const config = require("./webpack/config").default
 const variables = require("./assets/css/variables")
+const {
+	ResolverFactory,
+	NodeJsInputFileSystem,
+	CachedInputFileSystem
+} = require("enhanced-resolve")
+
+const CACHE_TTL = 6000
+
+const fileSystem = new CachedInputFileSystem(new NodeJsInputFileSystem(), CACHE_TTL)
+
+const resolver = ResolverFactory.createResolver(Object.assign({}, config.resolve || {}, {
+	fileSystem,
+	useSyncFileSystemCalls: true,
+	extensions: [ ".css" ]
+}))
 
 module.exports = {
 	plugins: {
 		"postcss-import": {
-			path: [ path.join(__dirname, "assets") ]
+			path: [ path.join(__dirname, "assets") ],
+			resolve(id, context) {
+				return resolver.resolveSync({}, context, id)
+			}
 		},
 		"postcss-url": {},
 		"postcss-cssnext": {

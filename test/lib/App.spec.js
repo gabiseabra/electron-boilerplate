b99c378f78d9e006f1b6888cab5629032ac6b1fa
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-09-11 23:11:17 -0300

    Added unit tests

---
+++ test/lib/App.spec.js
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+import path from "path"
+import semver from "semver-utils"
+import App from "../../src/lib/App"
+import pkg from "../../package.json"
+
+describe("App", () => {
+	const app = new App(path.join(__dirname, "../../"))
+
+	describe("#version", () => {
+		it("returns app version object", () => {
+			semver.stringify(app.version).should.equal(pkg.version)
+		})
+	})
+})
---
+++ test/setup.js
@@ -0,0 +1,5 @@
+import "babel-polyfill"
+import chai from "chai"
+
+global.should = chai.should()
+global.expect = chai.expect
